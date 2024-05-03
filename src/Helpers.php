2a1f7057ba93b166726f74f2ed7e27725761a2a2
MsNatali <MsNatali@users.noreply.github.com>
2024-05-03 11:33:46 +0000

    Fix styling

--- src/Helpers.php
+++ src/Helpers.php
@@ -4,16 +4,16 @@ namespace Ensi\LaravelPhpRdKafka;
 
 class Helpers
 {
-   public static function stringifyBoolean($value)
-   {
-      if ($value === true) {
-         return "true";
-      }
+    public static function stringifyBoolean($value)
+    {
+        if ($value === true) {
+            return "true";
+        }
 
-      if ($value === false) {
-         return "false";
-      }
+        if ($value === false) {
+            return "false";
+        }
 
-      return $value;
-   }
+        return $value;
+    }
 }
