733cbbfd9e755a55344348c8782c6b03270bdb68
C0rTeZ13 <work.cortez13@gmail.com>
2025-03-25 19:15:01 +0300

    Revert "ensitech-273 return to master realisation"
    This reverts commit 35fb4146ce8b9b6b65429e86552457b557e69e60.

--- src/Commands/CheckTopicsExistsCommand.php
+++ src/Commands/CheckTopicsExistsCommand.php
@@ -4,58 +4,60 @@ namespace Ensi\LaravelPhpRdKafka\Commands;
 
 use Ensi\LaravelPhpRdKafka\KafkaFacade;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Throwable;
 
 class CheckTopicsExistsCommand extends Command
 {
     protected $signature = 'kafka:find-not-created-topics
-                            {--validate : return error if there are not created topics}';
+                            {--validate : return error if there are not created topics}
+                            {--file= : path to file in which to write a list of non-existent topics}';
     protected $description = 'Check if all topics from kafka.topics exist';
-
-    public function __construct()
-    {
-        parent::__construct();
-        error_reporting(E_ALL & ~E_DEPRECATED);
-        ini_set('display_errors', '0');
-    }
 
     public function handle(): int
     {
-        $totalDesiredTopics = 0;
-        $notFoundTopics = [];
+        try {
+            $totalDesiredTopics = 0;
+            $notFoundTopics = [];
 
-        $connectionNames = KafkaFacade::availableConnections();
+            $connectionNames = KafkaFacade::availableConnections();
 
-        foreach ($connectionNames as $connectionName) {
-            $existingTopics = $this->getExistingTopics($connectionName);
+            foreach ($connectionNames as $connectionName) {
+                $existingTopics = $this->getExistingTopics($connectionName);
 
-            $desiredTopics = KafkaFacade::allTopics($connectionName);
-            $totalDesiredTopics += count($desiredTopics);
+                $desiredTopics = KafkaFacade::allTopics($connectionName);
+                $totalDesiredTopics += count($desiredTopics);
 
-            foreach ($desiredTopics as $topicName) {
-                if (!in_array($topicName, $existingTopics)) {
-                    $notFoundTopics[] = $topicName;
+                foreach ($desiredTopics as $topicName) {
+                    if (!in_array($topicName, $existingTopics)) {
+                        $notFoundTopics[] = $topicName;
+                    }
                 }
             }
-        }
 
-        if ($notFoundTopics) {
-            $this->output->writeln(join("\n", $notFoundTopics));
-        }
-
-        if ($this->option('validate')) {
             if ($notFoundTopics) {
-                $notFoundTopicsCount = count($notFoundTopics);
-                $this->output->writeln("\nThere are {$notFoundTopicsCount} not created topics");
-
-                return self::FAILURE;
-            } else {
-                $this->output->writeln("All {$totalDesiredTopics} desired topics exists");
-
-                return self::SUCCESS;
+                $this->writeOutput(join("\n", $notFoundTopics));
             }
-        }
 
-        return self::SUCCESS;
+            if ($this->option('validate')) {
+                if ($notFoundTopics) {
+                    $notFoundTopicsCount = count($notFoundTopics);
+                    $this->writeOutput("\nThere are {$notFoundTopicsCount} not created topics");
+
+                    return self::FAILURE;
+                } else {
+                    $this->writeOutput("All {$totalDesiredTopics} desired topics exist");
+
+                    return self::SUCCESS;
+                }
+            }
+
+            return self::SUCCESS;
+        } catch (Throwable $e) {
+            $this->writeOutput($e->getMessage());
+
+            return self::FAILURE;
+        }
     }
 
     private function getExistingTopics(string $connectionName): array
@@ -69,5 +71,16 @@ class CheckTopicsExistsCommand extends Command
         }
 
         return $existingTopics;
+    }
+
+    private function writeOutput(string $message): void
+    {
+        $filePath = $this->option('file');
+
+        if ($filePath) {
+            File::append($filePath, $message);
+        } else {
+            $this->output->writeln($message);
+        }
     }
 }
